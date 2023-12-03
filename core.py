2c04602680fbe4d23d6f7b5a8a5ec56e2e67e8c4
john <john>
2023-12-03 17:22:06 +0300

    First version of core.py

--- core.py
+++ core.py
@@ -1,61 +1,122 @@
 import numpy as np
-import sentinelhub
+import sentinelhub as sh
+
 
 class SentinelHubMaster:
-    to_dict = {}
+    # possible data_collections
+    TO_DATA_COLLECTION = {
+        "SENTINEL2_L1C": sh.data_collections.DataCollection.SENTINEL2_L1C,
+        "SENTINEL2_L2A": sh.data_collections.DataCollection.SENTINEL2_L2A,
+        "SENTINEL1": sh.data_collections.DataCollection.SENTINEL1,
+        "SENTINEL1_IW": sh.data_collections.DataCollection.SENTINEL1_IW,
+        "SENTINEL1_IW_ASC": sh.data_collections.DataCollection.SENTINEL1_IW_ASC,
+        "SENTINEL1_IW_DES": sh.data_collections.DataCollection.SENTINEL1_IW_DES,
+        "SENTINEL1_EW": sh.data_collections.DataCollection.SENTINEL1_EW,
+        "SENTINEL1_EW_ASC": sh.data_collections.DataCollection.SENTINEL1_EW_ASC,
+        "SENTINEL1_EW_DES": sh.data_collections.DataCollection.SENTINEL1_EW_DES,
+        "SENTINEL1_EW_SH": sh.data_collections.DataCollection.SENTINEL1_EW_SH,
+        "SENTINEL1_EW_SH_ASC": sh.data_collections.DataCollection.SENTINEL1_EW_SH_ASC,
+        "SENTINEL1_EW_SH_DES": sh.data_collections.DataCollection.SENTINEL1_EW_SH_DES,
+        "DEM": sh.data_collections.DataCollection.DEM,
+        "DEM_MAPZEN": sh.data_collections.DataCollection.DEM_MAPZEN,
+        "DEM_COPERNICUS_30": sh.data_collections.DataCollection.DEM_COPERNICUS_30,
+        "DEM_COPERNICUS_90": sh.data_collections.DataCollection.DEM_COPERNICUS_90,
+        "MODIS": sh.data_collections.DataCollection.MODIS,
+        "LANDSAT_MSS_L1": sh.data_collections.DataCollection.LANDSAT_MSS_L1,
+        "LANDSAT_TM_L1": sh.data_collections.DataCollection.LANDSAT_TM_L1,
+        "LANDSAT_TM_L2": sh.data_collections.DataCollection.LANDSAT_TM_L2,
+        "LANDSAT_ETM_L1": sh.data_collections.DataCollection.LANDSAT_ETM_L1,
+        "LANDSAT_ETM_L2": sh.data_collections.DataCollection.LANDSAT_ETM_L2,
+        "LANDSAT_OT_L1": sh.data_collections.DataCollection.LANDSAT_OT_L1,
+        "LANDSAT_OT_L2": sh.data_collections.DataCollection.LANDSAT_OT_L2,
+        "SENTINEL5P": sh.data_collections.DataCollection.SENTINEL5P,
+        "SENTINEL3_OLCI": sh.data_collections.DataCollection.SENTINEL3_OLCI,
+        "SENTINEL3_SLSTR": sh.data_collections.DataCollection.SENTINEL3_SLSTR,
+    }
 
     def __init__(
         self,
-        sh_client_id,
-        sh_client_secret,
+        sh_client_id: str,
+        sh_client_secret: str,
     ) -> None:
         if not sh_client_id or not sh_client_secret:
             raise ValueError("Some credentials are not provided")
-        self.config = sentinelhub.SHConfig(
+        self.config = sh.config.SHConfig(
             sh_client_id=sh_client_id,
             sh_client_secret=sh_client_secret,
             use_defaults=True,
         )
     
-    def get_box_array_image(
+    def get_box_tc_images(
         self,
         coords: tuple[float, float, float, float],
-        time_interval: tuple[str, str],
+        time_intervals: list[tuple[str, str]],
         data_collection: str,
-        resolution = 100, # "resolution" meters per pixel
-    ) -> np.ndarray:
-        n_bbox = sentinelhub.BBox(
+        erase_clouds: bool = False,
+        format: str = "png",
+        factor: float = 3.5,
+        resolution: float = 50,  # "resolution" meters per pixel
+    ) -> list[np.ndarray]:
+        n_bbox = sh.geometry.BBox(
             bbox=coords,
-            crs=sentinelhub.CRS.WGS84,
+            crs=sh.constants.CRS.WGS84,
         )
-        n_size = sentinelhub.bbox_to_dimensions(
+        n_size = sh.geo_utils.bbox_to_dimensions(
             n_bbox,
             resolution=resolution,
         )
-        request_true_color = sentinelhub.SentinelHubRequest(
-            evalscript=SentinelHubMaster.get_true_color_evalscript(
-                factor=3.5
-            ),
-            input_data=[
-                sentinelhub.SentinelHubRequest.input_data(
-                    data_collection=sentinelhub.DataCollection.SENTINEL2_L1C,
-                    time_interval=time_interval,
-                )
-            ],
-            responses=[
-                sentinelhub.SentinelHubRequest.output_response(
-                    "default",
-                    sentinelhub.MimeType.PNG,
-                ),
-            ],
-            bbox=n_bbox,
-            size=n_size,
+        if (erase_clouds):
+            cur_evalscript = SentinelHubMaster.get_true_color_black_clouds_evalscript(
+                factor=factor,
+            )
+        else:
+            cur_evalscript = SentinelHubMaster.get_true_color_evalscript(
+                factor=factor,
+            )
+        requests = []
+        for slot in time_intervals:
+            request_true_color = sh.api.process.SentinelHubRequest(
+                evalscript=cur_evalscript,
+                input_data=[
+                    sh.api.process.SentinelHubRequest.input_data(
+                        data_collection=SentinelHubMaster.TO_DATA_COLLECTION[
+                            data_collection.upper()
+                        ],
+                        time_interval=slot,
+                        mosaicking_order=sh.constants.MosaickingOrder.LEAST_CC,
+                    )
+                ],
+                responses=[
+                    sh.api.process.SentinelHubRequest.output_response(
+                        "default",
+                        format,
+                    ),
+                ],
+                bbox=n_bbox,
+                size=n_size,
+                config=self.config,
+            )
+            requests.append(request_true_color.download_list[0])
+        return sh.download.sentinelhub_client.SentinelHubDownloadClient(
             config=self.config,
-        )
-        return request_true_color.get_data()[0]
+        ).download(requests, max_threads=5)
     
     @staticmethod
-    def get_true_color_evalscript(factor = 2.5) -> str:
+    def get_pixel_size(
+        coords: tuple[float, float, float, float],
+        resolution: float = 50,
+    ) -> tuple[int, int]:
+        n_bbox = sh.geometry.BBox(
+            bbox=coords,
+            crs=sh.constants.CRS.WGS84,
+        )
+        return sh.geo_utils.bbox_to_dimensions(
+            n_bbox,
+            resolution=resolution
+        )
+
+    @staticmethod
+    def get_true_color_evalscript(factor: float = 3.5) -> str:
         return """
             //VERSION=3
             function setup() {{
@@ -72,5 +133,28 @@ class SentinelHubMaster:
                 return [sample.B04 * {0},
                         sample.B03 * {0},
                         sample.B02 * {0}];
+            }}
+        """.format(factor)
+    
+    @staticmethod
+    def get_true_color_black_clouds_evalscript(factor: float = 3.5) -> str:
+        return """
+            //VERSION=3
+            function setup() {{
+                return {{
+                    input: ["B02", "B03", "B04", "CLM"],
+                    output: {{ bands: 3 }}
+                }}
+            }}
+
+            function evaluatePixel(sample) {{
+                if (sample.CLM == 1) {{
+                    return [0, 0, 0]
+                }}
+                return [
+                    sample.B04 * {0},
+                    sample.B03 * {0},
+                    sample.B02 * {0},
+                ];
             }}
         """.format(factor)
--- main.py
+++ main.py
@@ -3,10 +3,11 @@ import shutil
 import pathlib
 import datetime
 import dotenv
-import sentinelhub
 import matplotlib.pyplot as plt
 import numpy as np
 import PIL.Image
+
+from core import SentinelHubMaster
 
 dotenv.load_dotenv(dotenv.find_dotenv())
 BASE_DIR = os.getcwd()
@@ -20,90 +21,41 @@ EXAMPLE_COORDS = (
     55.9740,
 ) # (Lng, Lat)
 
-def get_true_color_evalscript(factor = 2.5):
-    return """
-        //VERSION=3
-        function setup() {{
-            return {{
-                input: [{{
-                    bands: ["B02", "B03", "B04"]
-                }}],
-                output: {{
-                    bands: 3
-                }}
-            }};
-        }}
-        function evaluatePixel(sample) {{
-            return [sample.B04 * {0}, sample.B03 * {0}, sample.B02 * {0}];
-        }}
-    """.format(factor)
-
 def remove_folder_content(path: str):
     for filename in os.listdir(path):
         file_path = os.path.join(path, filename)
         if os.path.isfile(file_path) or os.path.islink(file_path):
             os.unlink(file_path)
 
-# def plot_image(
-#     image: np.ndarray,
-#     factor: float = 1.0,
-#     clip_range: tuple[float, float] | None = None,
-#     **kwargs,
-# ) -> None:
-#     """Utility function for plotting RGB images."""
-#     _, ax = plt.subplots(nrows=1, ncols=1, figsize=(15, 15))
-#     if clip_range is not None:
-#         ax.imshow(np.clip(image * factor, *clip_range), **kwargs)
-#     else:
-#         ax.imshow(image * factor, **kwargs)
-#     ax.set_xticks([])
-#     ax.set_yticks([])
-
 def main():
     remove_folder_content(OUTPUT_IMAGES_DIR)
-    config = sentinelhub.SHConfig(
+    time_intervals = [
+        ("2022-07-01", "2022-07-30"),
+        ("2022-08-01", "2022-08-30"),
+        ("2022-09-01", "2022-09-30"),
+        ("2022-10-01", "2022-10-30"),
+    ]
+
+    shmaster = SentinelHubMaster(
         sh_client_id=SH_CLIENT_ID,
         sh_client_secret=SH_CLIENT_SECRET,
-        use_defaults=True,
     )
-    if not config.sh_client_id or not config.sh_client_secret:
-        print("Some credentials are not provided")
-        return
-    resolution=60
-    n_bbox = sentinelhub.BBox(
-        bbox=EXAMPLE_COORDS,
-        crs=sentinelhub.CRS.WGS84,
-    )
-    n_size = sentinelhub.bbox_to_dimensions(
-        n_bbox,
-        resolution=resolution,
-    )
-    print("Size -> ", n_size)
-    request_true_color = sentinelhub.SentinelHubRequest(
-        evalscript=get_true_color_evalscript(factor=3.5),
-        input_data=[
-            sentinelhub.SentinelHubRequest.input_data(
-                data_collection=sentinelhub.DataCollection.SENTINEL2_L1C,
-                time_interval=("2023-07-11", "2023-07-14"),
-            )
-        ],
-        responses=[
-            sentinelhub.SentinelHubRequest.output_response(
-                "default",
-                sentinelhub.MimeType.PNG,
-            ),
-        ],
-        bbox=n_bbox,
-        size=n_size,
-        config=config,
-    )
-    true_color_image_arrays = request_true_color.get_data()
-    print("Length -> ", len(true_color_image_arrays))
-    for ind, image_array in enumerate(true_color_image_arrays):
-        save_image = PIL.Image.fromarray(image_array)
-        save_image.save(
-            os.path.join(OUTPUT_IMAGES_DIR, f"img_{ind}.png"),
-        )
+    print(SentinelHubMaster.get_pixel_size(
+        coords=EXAMPLE_COORDS,
+        resolution=40,
+    ))
+    # images = shmaster.get_box_tc_images(
+    #     coords=EXAMPLE_COORDS,
+    #     time_intervals=time_intervals,
+    #     data_collection="sentinel2_l1c",
+    #     erase_clouds=True,
+    #     resolution=40,
+    # )
+    # for ind, image_array in enumerate(images):
+    #     save_image = PIL.Image.fromarray(image_array)
+    #     save_image.save(
+    #         os.path.join(OUTPUT_IMAGES_DIR, f"img_{ind}.png"),
+    #     )
 
 if __name__ == "__main__":
     main()
